525ad29d1c66d14a39fa6b214504c0d7f75d9c7d
Frank Xin <frankxin@dhcp-10-105-57-197.wireless.northwestern.private>
2022-01-06 00:49:04 -0600

    Basic set up

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <nav className="w-full h-12 bg-background-900 flex">
+      <h1 className="text-neutral-50 font-black">MySwitchGames</h1>
+    </nav>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: {
+          100: "#3e3e3e",
+          900: "#323232",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
