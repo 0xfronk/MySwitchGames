0fb8a51f7facfa4b9804738c00476b3ec531c7b6
Frank Xin <frankxin@dhcp-10-105-57-197.wireless.northwestern.private>
2022-01-08 21:59:47 -0600

    Added game data

---
+++ src/data/games.js
@@ -0,0 +1,332 @@
+export const games = [
+  {
+    title: "Pokemon: Brilliant Diamond",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_70e00c08-0419-4274-9500-aefc47d50ecf",
+    genre: "RPG",
+  },
+  {
+    title: "New Super Mario Bros U Deluxe",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_5cbe0e37-54a3-4b51-bf0d-04f0d1d4b8c9",
+    genre: "Action",
+  },
+  {
+    title: "Just Dance 2022",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_fbfa9570-08c9-4e83-9f7e-614b319defdc",
+    genre: "Music & fitness",
+  },
+  {
+    title: "Just Dance 2021",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_4bab223c-4868-4dd0-929a-2db5a093a054",
+    genre: "Music & fitness",
+  },
+  {
+    title: "Just Dance 2020",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_613521ee-a244-4b54-8f43-eaeb620ead4a",
+    genre: "Music & fitness",
+  },
+  {
+    title: "Minecraft",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_2d29e86c-06b4-487b-8704-08efbc090bdf",
+    genre: "Simulation",
+  },
+  {
+    title: "Super Smash Bros. Ultimate",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_b26ea68d-b0c3-4f30-a67a-4182b17841f3",
+    genre: "Action",
+  },
+  {
+    title: "The Legend of Zelda: Breath of the Wild",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_e97f4ca6-9e20-4e34-ae64-1ac42d0d394c",
+    genre: "Adventure",
+  },
+  {
+    title: "Mario Kart 8 Deluxe",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_57bbec2f-28e8-40ab-885a-ae1976302fdd",
+    genre: "Sports & racing",
+  },
+  {
+    title: "Mario Golf: Super Rush",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_01fe99f2-a024-48f3-96db-81bfc3eb74cb",
+    genre: "Sports & racing",
+  },
+  {
+    title: "Pokemon Sword",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_550bae17-dc07-4a06-b726-64482954b101",
+    genre: "RPG",
+  },
+  {
+    title: "Mortal Kombat 11",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_13c31709-9dec-486f-84a7-805294a7196c",
+    genre: "Action",
+  },
+  {
+    title: "The Legend of Zelda: Skyward Sword HD",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_806bece1-6f00-439e-bba6-04be468f13aa",
+    genre: "Adventure",
+  },
+  {
+    title: "Animal Crossing: New Horizons",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_83f32699-98bf-49e0-903a-1eede0c034e8",
+    genre: "Simulation",
+  },
+  {
+    title: "Splatoon 2",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_9382109e-285f-4631-adaa-225fe82754e1",
+    genre: "Action",
+  },
+  {
+    title: "LEGO Marvel Super Heroes 2",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_9af2dfae-0de2-4aa7-9bc2-5cdb871df9e4",
+    genre: "Action",
+  },
+  {
+    title: "Overcooked! 2",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_19376d3a-2f3e-4bda-bb26-6f12b3d93efd",
+    genre: "Party",
+  },
+  {
+    title: "Super Mario Maker 2",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_03dcf89a-869d-4544-b5ae-1baa12bd84a5",
+    genre: "Action",
+  },
+  {
+    title: "Hasbro Game Night",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_36ee47b6-9181-4d82-a850-ed3fde8c179d",
+    genre: "Party",
+  },
+  {
+    title: "Cars 3: Driven to Win",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_f3dd511d-4470-4fdf-9f83-d0ca6c9e5576",
+    genre: "Sports & racing",
+  },
+  {
+    title: "Super Mario Party",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_0c8a0d59-dd86-43f3-a98a-b0d6439972ab",
+    genre: "Party",
+  },
+  {
+    title: "Super Mario Odyssey",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_0d151669-8465-4a77-b1d5-d0850f63c391",
+    genre: "Action",
+  },
+  {
+    title: "Luigi's Mansion 3",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_fd476c2c-b4ec-41f6-8c9d-a628fa7d24c3",
+    genre: "Action",
+  },
+  {
+    title: "FIFA 22: Legacy Edition",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_242414a9-7ce0-40e4-bdec-f817512fb305",
+    genre: "Sports & racing",
+  },
+  {
+    title: "Tony Hawk Pro Skater 1 + 2",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_4cf4e6a4-db26-4489-80cd-6a7e93ce650f",
+    genre: "Sports & racing",
+  },
+  {
+    title: "Yoshi's Crafted World",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_c6cfa1ff-82dd-4eaa-b688-f5fde94326c5",
+    genre: "Action",
+  },
+  {
+    title: "The Legend of Zelda: Link's Awakening",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_a98ecf27-8b81-42c1-bf55-81f6a42bc8f1",
+    genre: "Action",
+  },
+  {
+    title: "Miitopia",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_e4331568-df79-40e1-921e-3cee744e19e1",
+    genre: "Adventure",
+  },
+  {
+    title: "Game Builder Garage",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_dc2b1f19-7224-4da0-a0fd-98db92d1baae",
+    genre: "Education",
+  },
+  {
+    title: "Paper Mario",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_03200dfb-fa1f-4593-a54f-293adf2f10bc",
+    genre: "Adventure",
+  },
+  {
+    title: "Hades",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_29b75d47-e48d-4832-9914-99555d9dcc9c",
+    genre: "Action",
+  },
+  {
+    title: "Fire Emblem: Three Houses",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_0c91e7b3-d7be-417c-9838-af586d5c83c6",
+    genre: "Adventure",
+  },
+  {
+    title: "Stardew Valley",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_0923c845-4683-41a2-9def-a18cf34b716e",
+    genre: "Simulation",
+  },
+  {
+    title: "Pokemon: Shining Pearl",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_33fee4dc-299c-4805-bee5-c930cdcba953",
+    genre: "RPG",
+  },
+  {
+    title: "Kingdom Hearts: Melody of Memory",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_daf4cecd-7ae1-488f-8900-d41b880d5aab",
+    genre: "Music & fitness",
+  },
+  {
+    title: "Kirby Star Allies",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_ae2fe688-24a6-41b7-813d-2ca5c9eff087",
+    genre: "Action",
+  },
+  {
+    title: "Pokemon: Let's Go, Eevee!",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_2b100ecc-d0f6-4ddf-8852-e8196807bc6e",
+    genre: "RPG",
+  },
+  {
+    title: "Xenoblade Chronicles: Definitive Edition",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_ae687ca0-4027-4839-a40f-272199c33127",
+    genre: "RPG",
+  },
+  {
+    title: "Pokemon Shield",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_899c1b8e-7f3a-4b79-af4d-06e0baf062b1",
+    genre: "RPG",
+  },
+  {
+    title: "Pokemon: Let's Go, Pikachu!",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_95403b84-33b9-46d0-a614-89780147ec6c",
+    genre: "RPG",
+  },
+  {
+    title: "Hyrule Warriors: Age of Calamity",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_70524d70-49f3-40e2-9eff-0e31800016a6",
+    genre: "Action",
+  },
+  {
+    title: "Fitness Boxing 2: Rhythm & Exercise",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_8dcffff5-cbdb-4acd-bffe-4ef466ef5c12",
+    genre: "Sports & fitness",
+  },
+  {
+    title: "Mario + Rabbids: Kingdom Battle",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_6990fa5c-2b21-4e71-bf10-bc6bb49ba76e",
+    genre: "Adventure",
+  },
+  {
+    title: "Super Mario 3D All-Stars",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_c2f19539-056c-406d-bd99-70d2a97f4853",
+    genre: "Action",
+  },
+  {
+    title: "L.O.L. Surprise! Movie Night",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_82659ae4-1f98-490e-949c-627dbb6f6c4d",
+    genre: "Adventure",
+  },
+  {
+    title: "Cars 3: Driven to Win",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_f3dd511d-4470-4fdf-9f83-d0ca6c9e5576",
+    genre: "Sports & racing",
+  },
+  {
+    title: "Metroid Dread",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_059236e4-e115-4825-8814-b67e2d5b1010",
+    genre: "Action",
+  },
+  {
+    title: "NASCAR Heat: Ultimate Edition+",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_efc4b6ea-3b78-4828-932f-a868c3d5db66",
+    genre: "Sports & racing",
+  },
+  {
+    title: "NBA 2K22",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_4857630a-581d-4b10-8bb9-a0d04593e6f1",
+    genre: "Sports & racing",
+  },
+  {
+    title: "Crash Bandicoot N. Sane Trilogy",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_3a483937-cf62-4867-a859-648acafdf688",
+    genre: "Action",
+  },
+  {
+    title: "Clubhouse Games: 51 WorldWide Classics",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_6ab599ad-94da-406e-ab17-69500b94cd39",
+    genre: "Party",
+  },
+  {
+    title: "Cris Tales",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_ce1c0563-6d5e-4097-8729-9d2f9e2fde04",
+    genre: "RPG",
+  },
+  {
+    title: "Minecraft Dungeons: Ultimate Edition",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_7ab262e9-4616-44d5-92f0-7a106df4a10c",
+    genre: "Action",
+  },
+  {
+    title: "Spyro: Reignited Trilogy",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_ad9ddb68-04c4-47d0-b3e2-07a968147feb",
+    genre: "Adventure",
+  },
+  {
+    title: "Among Us: Crewmate Edition",
+    img_url:
+      "https://target.scene7.com/is/image/Target/GUEST_9b98c97e-86ee-4b24-9355-2b3f4cba9f6c",
+    genre: "Party",
+  },
+];
